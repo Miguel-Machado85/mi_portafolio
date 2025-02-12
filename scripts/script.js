766d3abe8bdff881386f9965606453f57916c73d
Miguel-Machado85 <miguel.machado8506@gmail.com>
2025-02-12 08:55:41 -0500

    init project

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function pelicula(){
+    const img = document.getElementById("cine")
+    img.setAttribute("src", "../resources/starwars.jpg")
+}
+
+function libro(){
+    const img = document.getElementById("libro")
+    img.setAttribute("src", "../resources/irene.webp")
+}
